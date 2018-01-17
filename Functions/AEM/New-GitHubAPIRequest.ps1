a08dcf112563e92f8a68319170fda227059c60b3
Phil Greene - (SEA) <philg@chartercon.com>
2018-01-16 17:05:59 -0800

    Adding AEM Function for powershell testing. -Phil

---
+++ Functions/AEM/New-GitHubAPIRequest.ps1
@@ -0,0 +1,25 @@
+﻿Function New-GitHubAPIRequest
+    {
+        Param(
+            [Parameter(Mandatory=$true)]$URL,
+            [Parameter(Mandatory=$true)]$Token,
+            [Parameter(Mandatory=$false)]$FilePath
+        )
+        $params = @{
+                Uri         =	$URL
+		        Method      =	'GET'
+		        ContentType	= 	'application/vnd.github.v3+json'
+		        Headers     =	@{ 'Authorization'= 'token ' + $Token }
+	        }
+            
+            If($FilePath -eq $null){
+                Try{ $Response = Invoke-WebRequest @params }Catch{ "Error with Invoke-WebRequest!" | Write }
+                $Content = ConvertFrom-JSON($Response.Content)
+                Return $Content
+            }
+            Else{
+                Invoke-WebRequest @params | Out-File -FilePath $FilePath            
+                If($?){ $Content = $FilePath + (Test-Path $FilePath).ToString() }
+            }            
+            Return $Content
+        }
